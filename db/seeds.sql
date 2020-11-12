fa622b2d44d1a9d7a57f5e6a6cea8e8323f92c1b
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-11-12 14:57:25 +1100

    added code to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Southern Fried Chicken Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beef and Bacon Burger", false);
